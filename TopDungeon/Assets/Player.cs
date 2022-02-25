bbd66537cd4ad8d2fced297989aac2ebd96abb90
palindungan <rizkika.palindungan1@gmail.com>
2022-02-25 18:17:18 +0700

    player script

---
+++ TopDungeon/Assets/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // Reset MoveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        // Swab sprite direction, wether you're going right or left
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // Make this thing move!
+        transform.Translate(moveDelta * Time.deltaTime);
+    }
+}
