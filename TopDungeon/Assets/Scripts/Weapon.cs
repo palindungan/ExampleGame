9bc6d780ba31c927df1a37d32c8d54cc8ad3b4d8
palindungan <rizkika.palindungan1@gmail.com>
2022-03-20 20:39:46 +0700

    scripting weapon

--- TopDungeon/Assets/Scripts/Weapon.cs
+++ TopDungeon/Assets/Scripts/Weapon.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public class Weapon : Collidable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    // Damage Struct
+    public int damagePoint = 1;
+    public float pushForce = 2.0f;
 
+    // Upgrade
+    public int weaponLevel = 0;
+    private SpriteRenderer spriteRenderer;
+
+    private float coolDown = 0.5f;
+    private float lastSwing;
+
+    protected override void Start()
+    {
+        base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
+        base.Update();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.time - lastSwing > coolDown)
+            {
+                lastSwing = Time.time;
+                Swing();
+            }
+        }
+    }
 
+    private void Swing()
+    {
+        //
+        Debug.Log("Swing");
     }
 }
