d485edac8d6506da362d9a906ceeff89ece5770f
palindungan <rizkika.palindungan1@gmail.com>
2022-03-21 22:08:17 +0700

    fighter + enemy init c#

---
+++ TopDungeon/Assets/Scripts/Fighter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    // public fields
+    public int hitPoint = 10;
+    public int maxHitPoint = 10;
+    public float pushRecoverySpeed = 0.2f;
+
+    // immunity
+    protected float immuneTime = 1.0f;
+    protected float lastImmune;
+
+    // push
+    protected Vector3 pushDirection;
+
+    // all fighters can RecieveDamage / Die
+    protected virtual void ReceiveDamage(Damage dmg)
+    {
+        if (Time.time - lastImmune > immuneTime)
+        {
+            lastImmune = Time.time;
+            hitPoint -= dmg.damageAmount;
+            pushDirection = (transform.position - dmg.origin).normalized * dmg.pushForce;
+
+            GameManager.instance.ShowText(dmg.damageAmount.ToString, 15, Color.red, transform.position, Vector3.zero, 0.5f);
+
+            if (hitPoint <= 0)
+            {
+                hitPoint = 0;
+                Death();
+            }
+        }
+    }
+
+    protected virtual void Death()
+    {
+
+    }
+}
